d21605b14414a2d21dc15092e2b57a1c2bb8f766
marce.ortiz11 <marceloortiz82@gmail.com>
2023-12-23 11:03:06 -0300

    2da entrega

--- main.js
+++ main.js
@@ -1,37 +1,59 @@
-// En principio, la idea es armar un sistema para emitir turnos en un Consultorio Medico que me gustaria adaptar para organizar la agenda de un Profesional
+//La idea sigue siendo la misma, un emisor de turnos que luego permita ordenarlos de alguna manera, por el momento es alfabeticamente segun nombre. 
+//Se agrego una validacion para la emision de turnos ante campos vacios o nulos, segun la correccion de la entrega anterior.
+//Quizas para las proximas entregas modifique el proyecto entero.
+
 
 // Solicitar validación de usuario
-let usuarioValido = prompt("¡Hola! Por favor, ingrese su usuario: (user: tutor coder)");
+let usuarioValido = prompt("¡Hola! Por favor, ingrese su nombre para validar: (user: secretaria)");
 
-// Validar el nombre del usuario (se puede cambiar "tutor" por un nombre)
-if (usuarioValido && usuarioValido.toLowerCase() === "tutor coder") {
-    alert("¡Bienvenido, Tutor Coder! Estás validado para emitir turnos.");
+// Validar el nombre del usuario (usuario "secretaria")
+if (usuarioValido && usuarioValido.toLowerCase() === "secretaria") {
+    alert("¡Bienvenida, Secretaria! Estás validada para emitir turnos.");
     
-    // Inician variables
+    // Inicializamos variables
     let listaTurnos = [];
-    let tiempoEsperaTotal = 0;
+    let tiempoEsperaTotal = 25;
 
     // Función para calcular el tiempo de espera en minutos
     function calcularTiempoEspera(turnoActual) {
-        return turnoActual * 5; // Se puso 5´como tiempo estimado de demora
+        return turnoActual * 5; // Tiempo entre turnos 5´
     }
 
-    // Ciclo interactivo para emitir hasta CINCO turnos
+    // Función para reordenar los turnos alfabéticamente
+    function reordenarTurnos(turnos) {
+        return turnos.sort();
+    }
+
+    // Ciclo interactivo para emitir CINCO turnos
     for (let i = 1; i <= 5; i++) {
-        let nuevoTurno = prompt("Ingrese el nombre del cliente para el turno #" + i);
-        listaTurnos.push(nuevoTurno);
+        let nuevoTurno = prompt(`Ingrese el nombre del paciente para el turno #${i}`);
 
-        // Alerta con el turno y tiempo de espera
-        alert(`Turno emitido: ${nuevoTurno}\nTiempo de espera: ${calcularTiempoEspera(i)} minutos`);
+        // Validar nombre no vacío y no nulo
+        while (!nuevoTurno) {
+            nuevoTurno = prompt("Por favor, ingrese un nombre válido para el turno #" + i);
+        }
 
-        // Suma al tiempo de espera total
-        tiempoEsperaTotal += calcularTiempoEspera(i);
+        listaTurnos.push({ numero: i, nombre: nuevoTurno, tiempoEspera: calcularTiempoEspera(i) });
+
+        // Mostrar alerta con el turno y tiempo de espera
+        alert("Turno emitido: " + nuevoTurno + "\nTiempo de espera: " + calcularTiempoEspera(i) + " minutos");
     }
 
-    // Muestra resumen al final para dar una idea de cierre del algoritmo
-    alert(`Resumen:\nTurnos emitidos: ${listaTurnos.join(", ")}\nTiempo de espera total: ${tiempoEsperaTotal} minutos`);
+    // Reordenar los turnos alfabéticamente por el nombre
+    listaTurnos.sort((a, b) => a.nombre.localeCompare(b.nombre));
+
+    // Función de orden superior para mostrar información de los turnos
+    function mostrarInformacionTurnos(turno, indice) {
+        alert(`Turno #${turno.numero}: ${turno.nombre}\nTiempo de espera: ${turno.tiempoEspera} minutos`);
+    }
+
+    // Mostrar información de los turnos utilizando la función de orden superior
+    listaTurnos.forEach(mostrarInformacionTurnos);
+
+    // Mostrar resumen al final
+    alert(`Resumen:\nTurnos emitidos: ${listaTurnos.map(turno => turno.nombre).join(", ")}\nTiempo de espera total: ${tiempoEsperaTotal} minutos`);
 
 } else {
-    // Si el nombre no es válido, muestra un mensaje de error
+    // Si el nombre no es válido, mostrar un mensaje de error
     alert("Usuario no válido. Acceso denegado.");
 }
